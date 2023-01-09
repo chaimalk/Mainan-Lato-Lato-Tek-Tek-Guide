9a0a850b31d4e27afb34d3ac92f5933924b1d7ee
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-01-10 00:03:50 +0100

    Create lato.ads

---
+++ lato.ads
@@ -0,0 +1,55 @@
+{
+	"Playstore": {
+
+		"AdsController": {
+
+			"NetworkAds": "admob",
+
+			"BannerAdmob": "ca-app-pub-7448804041969308/6003527069",
+			"InterstitialAdmob": "ca-app-pub-7448804041969308/4498873709",
+
+			"BannerFacebook": "594930265627136_594930765627086",
+			"InterstitialFacebook": "594930265627136_594931265627036",
+			
+			"unityGameID": "5111929",
+			"UnityPlacement": "Interstitial_Android",
+
+			"StartappAds": "200715477",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": true,
+			"Rating": true
+
+		}
+
+	},
+	"Huaweistore": {
+
+		"AdsController": {
+
+			"NetworkAds": "startapp",
+
+			"BannerAdmob": "ca-app-pub-8082149907872048/3898325980",
+			"InterstitialAdmob": "ca-app-pub-8082149907872048/8781442418",
+
+			"BannerFacebook": "1347348165628536_1347349145628438",
+			"InterstitialFacebook": "1347348165628536_1347353132294706",
+
+			"StartappAds": "211203742",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": false,
+			"Rating": false
+
+		}
+
+	}
+}

da114961c235b8828fb1c6d1d3f90b713821e5d8
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-20 15:39:38 +0100

    Update lato.ads

--- lato.ads
+++ lato.ads
@@ -5,8 +5,8 @@
 
 			"NetworkAds": "unity",
 
-			"BannerAdmob": "ca-app-pub-1010770342437944/8003805455",
-			"InterstitialAdmob": "ca-app-pub-1010770342437944/9002844745",
+			"BannerAdmob": "ca-app-pub-1010770342437944/8003805400",
+			"InterstitialAdmob": "ca-app-pub-1010770342437944/9002844700",
 
 			"BannerFacebook": "594930265627136_594930765627000",
 			"InterstitialFacebook": "594930265627136_594931265627000",

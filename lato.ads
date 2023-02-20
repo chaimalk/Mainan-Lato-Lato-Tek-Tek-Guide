f72317a68ab0d269bafba714a79840a64fa87278
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-20 15:33:55 +0100

    Update lato.ads

--- lato.ads
+++ lato.ads
@@ -3,7 +3,7 @@
 
 		"AdsController": {
 
-			"NetworkAds": "admob",
+			"NetworkAds": "unity",
 
 			"BannerAdmob": "ca-app-pub-1010770342437944/8003805455",
 			"InterstitialAdmob": "ca-app-pub-1010770342437944/9002844745",
